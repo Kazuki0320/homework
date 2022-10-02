d1337fa039cd56412997d917dfa8cb23fe41e516
Kazuki.T <bad0320@icloud.com>
2022-10-02 15:18:59 +0900

    Todoapp2

--- jsprimer/Todoapp1/main.js
+++ jsprimer/Todoapp1/main.js
@@ -3,28 +3,28 @@ const taskSubmit = document.getElementsByClassName('task_submit')[0];
 const taskList = document.getElementsByClassName('task_list')[0];
 
 const addTasks = (task) => {
-	const listItem = document.createElement('li');//createElementで引数の要素を作成
-	const showItem = taskList.appendChild(listItem);//appendChildで、親要素に子要素を追加する
-	showItem.innerHTML = task;//入力したタスクを表示する
+	const listItem = document.createElement('li');
+	const showItem = taskList.appendChild(listItem);
+	showItem.innerHTML = task;
 
 	const deleteButton = document.createElement('button');
-	deleteButton.innerHTML = 'Delete';//createElementで作成したbuttonの表示を'Delete'とする
-	listItem.appendChild(deleteButton);//listItemに子要素として、deleteButtonを追加
+	deleteButton.innerHTML = 'Delete';
+	listItem.appendChild(deleteButton);
 
-	deleteButton.addEventListener('click', evt => {//deleteButtonが呼び出された時に、イベントが発火するようにする
+	deleteButton.addEventListener('click', evt => {
 		evt.preventDefault();
-		deleteTasks(deleteButton);//deleteTasksに,イベントの処理を書いていく
+		deleteTasks(deleteButton);
 	});
 };
 
 const deleteTasks = (deleteButton) => {
-	const chosenTask = deleteButton.closest('li');//親要素と自身の要素から一致する要素をかえす
-	taskList.removeChild(chosenTask);//一致した要素をremoveChildで削除する処理を書く
+	const chosenTask = deleteButton.closest('li');
+	taskList.removeChild(chosenTask);
 };
 
-taskSubmit.addEventListener('click', evt => {//クリックされた時に、submitにもイベントを付与
+taskSubmit.addEventListener('click', evt => {
 	evt.preventDefault();
-	const task = taskValue.value;//taskにtaskValue.valueを代入することで、addTasks関数が呼び出された時に、文字入力をリセットする
+	const task = taskValue.value;
 	addTasks(task);
 	taskValue.value = '';
 });
