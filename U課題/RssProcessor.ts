5d5093048f14be3d4736cedd38eba89b857fabef
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-06-05 22:54:45 +0900

    クラスを宣言し、責務の分離

---
+++ U課題/RssProcessor.ts
@@ -0,0 +1,60 @@
+/**
+ * Rssフィールドから”NewsPicks”という文字列を取り除き、結果を出力する問題
+ * 
+ * 必要な処理
+ * ・ 全ての処理を管理する
+ * ・ RSSフィールドを読み込む
+ * ・ ファイルの中身が正しいかチェック
+ * ・ "NewsPicks"の文字列を取り除く
+ * ・ 標準出力
+ */
+
+/**
+ * 必要なクラス
+ * ・ RssProcessor
+ * ・ RssReader
+ * ・ RssChecker
+ * ・ RssTrim
+ * ・ RssOutput
+ * ・ RssItem
+ */
+class RssItem {
+
+}
+
+class RssFetcher {
+
+}
+
+class RssParser {
+
+}
+
+class RssValidator {
+
+}
+
+class RssCleaner {
+
+}
+
+interface OutputStrategyInterface {
+
+}
+
+class RssOutput {
+
+}
+
+class RssService {
+	private fetcher: RssFetcher;
+	private parser: RssParser;
+	private validator: RssValidator;
+	private cleaner: RssCleaner;
+	private outputStrategy: OutputStrategyInterface;
+
+
+	__construct() {
+		
+	}
+}
